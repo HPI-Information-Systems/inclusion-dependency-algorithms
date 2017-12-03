79fef1b2d19277f480a74b80409334889d2b7cd7
Falco Duersch <falco.duersch@student.hpi.de>
2017-12-03 12:44:38 +0100

    Refactor Spider into Spider plus common (test) util project

---
+++ adp-algorithms/util/src/test/java/de/metanome/util/TableInputGeneratorStub.java
@@ -0,0 +1,88 @@
+package de.metanome.util;
+
+import static java.util.stream.Collectors.toList;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import de.metanome.algorithm_integration.AlgorithmConfigurationException;
+import de.metanome.algorithm_integration.input.InputGenerationException;
+import de.metanome.algorithm_integration.input.RelationalInput;
+import de.metanome.algorithm_integration.input.TableInputGenerator;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Comparator;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Singular;
+import org.mockito.internal.stubbing.answers.ReturnsElementsOf;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+public class TableInputGeneratorStub implements TableInputGenerator {
+
+  private final List<String> columnNames;
+  private final List<Row> rows;
+
+  @Builder
+  public TableInputGeneratorStub(@Singular final List<String> columnNames,
+      @Singular final List<Row> rows) {
+    this.columnNames = columnNames;
+    this.rows = rows;
+  }
+
+  @Override
+  public ResultSet sortBy(String column, Boolean descending)
+      throws InputGenerationException, AlgorithmConfigurationException {
+
+    final int index = columnNames.indexOf(column);
+    final List<String> values = rows.stream()
+        .map(r -> r.getValues().get(index))
+        .sorted(Comparator.nullsFirst(Comparator.naturalOrder()))
+        .collect(toList());
+
+    try {
+      final ResultSet resultSet = mock(ResultSet.class);
+      given(resultSet.next()).willAnswer(new NextAnswer(values.size()));
+      given(resultSet.getString(column)).willAnswer(new ReturnsElementsOf(values));
+      return resultSet;
+    } catch (final SQLException e) {
+      throw new InputGenerationException("Error", e);
+    }
+  }
+
+  @AllArgsConstructor
+  private static class NextAnswer implements Answer<Boolean> {
+
+    private int counter;
+
+    @Override
+    public Boolean answer(final InvocationOnMock invocation) throws Throwable {
+      return counter-- != 0;
+    }
+  }
+
+  @Override
+  public ResultSet filter(String filterExpression)
+      throws InputGenerationException, AlgorithmConfigurationException {
+
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public ResultSet select() throws InputGenerationException, AlgorithmConfigurationException {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public RelationalInput generateNewCopy()
+      throws InputGenerationException, AlgorithmConfigurationException {
+
+    return new RelationalInputStub(columnNames, rows);
+  }
+
+  @Override
+  public void close() throws Exception {
+    // no-op
+  }
+}
