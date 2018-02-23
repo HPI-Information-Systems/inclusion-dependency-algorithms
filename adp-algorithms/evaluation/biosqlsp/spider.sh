946e491cfcfe37c45a31f4e6911e9858c1007444
Falco Duersch <falco.duersch@student.hpi.de>
2018-02-23 09:37:30 +0100

    Add some BioSQLSP scriptAdd some BioSQLSP scriptss

---
+++ adp-algorithms/evaluation/biosqlsp/spider.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export EXECUTION_ID="biosqlsp_spider"$@
+
+java $DEBUG $JVM_ARGS \
+-cp $ADP_LIB:$ALGORITHMS/spider/build/libs/spider-0.1.0-SNAPSHOT-database.jar \
+de.metanome.cli.App \
+--algorithm de.metanome.algorithms.spider.SpiderDatabaseAlgorithm \
+$DB \
+--table-key TABLE \
+--tables load:biosqlsp/biosqlsp.txt \
+--algorithm-config PROCESS_EMPTY_COLUMNS:true,
+--algorithm-config INPUT_ROW_LIMIT:-1,MAX_MEMORY_USAGE_PERCENTAGE:50,MEMORY_CHECK_INTERVAL:1000 \
+--output file:$EXECUTION_ID
