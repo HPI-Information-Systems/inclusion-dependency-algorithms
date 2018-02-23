64afa393d69d67ee509ab8f8cfcb5f2d9d836a7f
Falco Duersch <falco.duersch@student.hpi.de>
2018-02-23 09:05:41 +0100

    Move result comparision script to evaluation and enhance it

--- adp-algorithms/evaluation/compare_results.py
+++ adp-algorithms/evaluation/compare_results.py
@@ -3,12 +3,27 @@
 import json
 import sys
 
+"""
+Compare two output files of Metanome.
+
+Print information on how many INDs were found and print some example INDs which
+were found in only one of the sets.
+In order to compare output files of algorithms that were run on databases and
+CSV files,
+
+(1) the ".csv" suffix of relation names is truncated,
+(2) the identifiers are compared case-insensitively.
+"""
+
 
 def ind_to_str(ind):
     assert ind['type'] == 'InclusionDependency'
 
+    def _table(n):
+        return n.rsplit(".", 1)[0] if n.endswith('.csv') else n
+
     def _join_columns(columns):
-        names = [col['tableIdentifier'] + '.' + col['columnIdentifier']
+        names = [_table(col['tableIdentifier']) + '.' + col['columnIdentifier']
                  for col in columns]
         return ','.join(names)
 
@@ -20,7 +35,7 @@ def parse_ind(filename):
     with open(filename, 'r') as f:
         result = []
         for line in f.readlines():
-            result.append(ind_to_str(json.loads(line)))
+            result.append(ind_to_str(json.loads(line)).lower())
         return result
 
 
@@ -36,6 +51,10 @@ def print_difference(file1, ind1, file2, ind2):
 
 
 def main():
+    if len(sys.argv) < 3:
+        print("Usage: python compare_result.py file1 file2")
+        exit()
+
     file1 = sys.argv[1]
     file2 = sys.argv[2]
 
