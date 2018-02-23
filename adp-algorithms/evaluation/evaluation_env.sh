8d31572a1755978c4eab109c074624d31a5ee113
Falco Duersch <falco.duersch@student.hpi.de>
2018-02-23 10:22:21 +0100

    Adjust RAM size for keket

--- adp-algorithms/evaluation/evaluation_env.sh
+++ adp-algorithms/evaluation/evaluation_env.sh
@@ -3,7 +3,7 @@ METANOME_CLI="metanome-cli-1.1.1-SNAPSHOT.jar"
 POSTGRES="postgresql-42.2.1.jar"
 ADP_LIB=$METANOME_CLI:$POSTGRES
 ALGORITHMS=".."
-JVM_ARGS="-Xmx4g -Dtinylog.configuration=tinylog-evaluation.properties"
+JVM_ARGS="-Xmx83g -Dtinylog.configuration=tinylog-evaluation.properties"
 DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 DB="--db-type postgresql --db-connection pgpass.conf"
 set +a
