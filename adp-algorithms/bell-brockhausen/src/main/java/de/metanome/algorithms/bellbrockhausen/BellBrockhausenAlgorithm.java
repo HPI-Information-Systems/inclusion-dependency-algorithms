fcc7b94ae97eceae03c586c057bf09ebf854a586
Falco Duersch <falco.duersch@student.hpi.de>
2018-02-24 11:33:59 +0100

    Accept multiple tables as TableInput in BB
    Previously only a single relation name was
    passed as string list which is makes passing
    relation names with Metanome CLI harder
    than it needs to be.

--- adp-algorithms/bell-brockhausen/src/main/java/de/metanome/algorithms/bellbrockhausen/BellBrockhausenAlgorithm.java
+++ adp-algorithms/bell-brockhausen/src/main/java/de/metanome/algorithms/bellbrockhausen/BellBrockhausenAlgorithm.java
@@ -1,29 +1,28 @@
 package de.metanome.algorithms.bellbrockhausen;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableList;
+import static de.metanome.algorithms.bellbrockhausen.configuration.ConfigurationKey.DATABASE;
+import static de.metanome.algorithms.bellbrockhausen.configuration.ConfigurationKey.TABLE;
+
+import de.metanome.algorithm_integration.AlgorithmConfigurationException;
 import de.metanome.algorithm_integration.AlgorithmExecutionException;
 import de.metanome.algorithm_integration.algorithm_types.DatabaseConnectionParameterAlgorithm;
 import de.metanome.algorithm_integration.algorithm_types.InclusionDependencyAlgorithm;
-import de.metanome.algorithm_integration.algorithm_types.StringParameterAlgorithm;
+import de.metanome.algorithm_integration.algorithm_types.TableInputParameterAlgorithm;
 import de.metanome.algorithm_integration.configuration.ConfigurationRequirement;
 import de.metanome.algorithm_integration.configuration.ConfigurationRequirementDatabaseConnection;
-import de.metanome.algorithm_integration.configuration.ConfigurationRequirementString;
+import de.metanome.algorithm_integration.configuration.ConfigurationRequirementTableInput;
 import de.metanome.algorithm_integration.input.DatabaseConnectionGenerator;
+import de.metanome.algorithm_integration.input.RelationalInput;
+import de.metanome.algorithm_integration.input.TableInputGenerator;
 import de.metanome.algorithm_integration.result_receiver.InclusionDependencyResultReceiver;
 import de.metanome.algorithms.bellbrockhausen.accessors.DataAccessObject;
 import de.metanome.algorithms.bellbrockhausen.accessors.PostgresDataAccessObject;
 import de.metanome.algorithms.bellbrockhausen.configuration.BellBrockhausenConfiguration;
 import de.metanome.algorithms.bellbrockhausen.configuration.BellBrockhausenConfiguration.BellBrockhausenConfigurationBuilder;
-
 import java.util.ArrayList;
-import java.util.List;
 
-import static de.metanome.algorithms.bellbrockhausen.configuration.ConfigurationKey.DATABASE;
-import static de.metanome.algorithms.bellbrockhausen.configuration.ConfigurationKey.TABLE;
-
-public class BellBrockhausenAlgorithm implements InclusionDependencyAlgorithm,
-        DatabaseConnectionParameterAlgorithm, StringParameterAlgorithm {
+public class BellBrockhausenAlgorithm implements InclusionDependencyAlgorithm, DatabaseConnectionParameterAlgorithm,
+    TableInputParameterAlgorithm {
 
     private final BellBrockhausenConfigurationBuilder configurationBuilder;
 
@@ -35,22 +34,30 @@ public class BellBrockhausenAlgorithm implements InclusionDependencyAlgorithm,
     public ArrayList<ConfigurationRequirement<?>> getConfigurationRequirements() {
         final ArrayList<ConfigurationRequirement<?>> requirements = new ArrayList<>();
         requirements.add(new ConfigurationRequirementDatabaseConnection(DATABASE.name()));
-        requirements.add(new ConfigurationRequirementString(TABLE.name()));
+        requirements.add(new ConfigurationRequirementTableInput(TABLE.name()));
         return requirements;
     }
 
-    @Override
+  @Override
+  public void setTableInputConfigurationValue(final String identifier, final TableInputGenerator... values)
+  throws AlgorithmConfigurationException {
+
+    if (identifier.equals(TABLE.name())) {
+      for (final TableInputGenerator generator : values) {
+        try (RelationalInput input = generator.generateNewCopy()) {
+          configurationBuilder.tableName(input.relationName());
+        } catch (final Exception e) {
+          throw new AlgorithmConfigurationException("cannot get table name", e);
+        }
+      }
+    }
+  }
+
+  @Override
     public void setDatabaseConnectionGeneratorConfigurationValue(
             String identifier, DatabaseConnectionGenerator... values) {
         if (identifier.equals(DATABASE.name())) {
             configurationBuilder.connectionGenerator(values[0]);
-        }
-    }
-
-    @Override
-    public void setStringConfigurationValue(String identifier, String... values) {
-        if (identifier.equals(TABLE.name())) {
-            configurationBuilder.tableName(values[0]);
         }
     }
 
