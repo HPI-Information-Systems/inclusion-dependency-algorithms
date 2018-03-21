3fd7fb258433c80f306bd4d92efbe2907b097005
Falco Duersch <falco.duersch@student.hpi.de>
2018-03-21 10:42:18 +0100

    Quick fix BINDER: do not accumulate DB connections endlessly

--- adp-algorithms/binder/src/main/java/de/metanome/algorithms/binder/io/FileInputIterator.java
+++ adp-algorithms/binder/src/main/java/de/metanome/algorithms/binder/io/FileInputIterator.java
@@ -1,40 +1,42 @@
 package de.metanome.algorithms.binder.io;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.metanome.algorithm_integration.AlgorithmConfigurationException;
 import de.metanome.algorithm_integration.input.InputGenerationException;
 import de.metanome.algorithm_integration.input.InputIterationException;
 import de.metanome.algorithm_integration.input.RelationalInput;
 import de.metanome.algorithm_integration.input.RelationalInputGenerator;
+import de.metanome.algorithm_integration.input.TableInputGenerator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileInputIterator implements InputIterator {
 
-	private RelationalInput inputGenerator = null;
-	private List<String> record = null;
-	
+	private RelationalInputGenerator inputGenerator;
+	private RelationalInput relationalInput;
+	private List<String> record;
+
 	private int rowsRead = 0;
 	private int inputRowLimit;
-	
+
 	public FileInputIterator(RelationalInputGenerator inputGenerator, int inputRowLimit) throws InputGenerationException, AlgorithmConfigurationException {
-		this.inputGenerator = inputGenerator.generateNewCopy();
+		this.inputGenerator = inputGenerator;
+		this.relationalInput = inputGenerator.generateNewCopy();
 		this.inputRowLimit = inputRowLimit;
 	}
-	
+
 	@Override
 	public boolean next() throws InputIterationException {
-		if (this.inputGenerator.hasNext() && ((this.inputRowLimit <= 0) || (this.rowsRead < this.inputRowLimit))) {
-			List<String> input = this.inputGenerator.next();
+		if (this.relationalInput.hasNext() && ((this.inputRowLimit <= 0) || (this.rowsRead < this.inputRowLimit))) {
+			List<String> input = this.relationalInput.next();
 			this.record = new ArrayList<>(input.size());
-			
+
 			for (String value : input) {
 				// Replace line breaks with the zero-character, because these line breaks would otherwise split values when later written to plane-text buckets
 				if (value != null)
 					value = value.replaceAll("\n", "\0");
 				this.record.add(value);
 			}
-			
+
 			this.rowsRead++;
 			return true;
 		}
@@ -53,6 +55,10 @@ public class FileInputIterator implements InputIterator {
 
 	@Override
 	public void close() throws Exception {
-		this.inputGenerator.close();
+		this.relationalInput.close();
+
+		if (inputGenerator instanceof TableInputGenerator) {
+			this.inputGenerator.close();
+		}
 	}
 }
