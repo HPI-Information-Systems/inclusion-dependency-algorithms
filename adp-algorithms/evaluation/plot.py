e8aa10611aab071222175f1ecf0fa33a09684dba
Falco Duersch <falco.duersch@student.hpi.de>
2018-05-07 10:48:40 +0200

    Version of plotting script used for the seminar paper

--- adp-algorithms/evaluation/plot.py
+++ adp-algorithms/evaluation/plot.py
@@ -10,22 +10,42 @@ from matplotlib.ticker import FuncFormatter
 
 algorithms = ['demarchi', 'spider', 'sindd', 'bb', 'binder']
 algorithm_names = ['DeMarchi', 'Spider', 'S-INDD', 'BellBrockhausen', 'BINDER']
-datasets = ['scop', 'biosql', 'cath', 'tesma']
+
+# Colors from the default property cycle
+# https://matplotlib.org/gallery/color/color_cycle_default.html
+algorithm_colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:len(algorithms)]
+algorithm_to_color = dict(zip(algorithms, algorithm_colors))
+
+# https://matplotlib.org/api/markers_api.html#module-matplotlib.markers
+algorithm_markers = ['D', 'v', 'o', '^', 's']
+algorithm_to_marker = dict(zip(algorithms, algorithm_markers))
+
+
+def read_dataset(name):
+    raw = pd.read_csv(name,
+                      header=None,
+                      index_col=0)
+    return raw.T.reindex(columns=algorithms)
+
+
+scop = read_dataset('scop.csv')
+biosql = read_dataset('biosql.csv')
+cath = read_dataset('cath.csv')
+tesma = read_dataset('tesma.csv')
+musicbrainz = read_dataset('musicbrainz.csv')
+wikirank = read_dataset('wikirank.csv')
+wikipedia = read_dataset('wikipedia.csv')
+lod = read_dataset('lod.csv')
+ensembl = read_dataset('ensembl.csv')
+census = read_dataset('census.csv')
+tpch1 = read_dataset('tpch1s.csv')
+tpch10 = read_dataset('tpch10s.csv')
+
+datasets = ['scop', 'biosql', 'cath', 'tesma', 'musicbrainz', 'wikirank', 'wikipedia', 'lod', 'ensembl', 'census', 'tpc-h 1', 'tpc-h 10']
+dataset_list = [scop, biosql, cath, tesma, musicbrainz, wikirank, wikipedia, lod, ensembl, census, tpch1, tpch10]
 dataset_names = [d.upper() for d in datasets]
-
-
-scop = pd.read_csv('scop.csv', header=None, index_col=0).T.reindex(columns=algorithms)
-biosql = pd.read_csv('biosql.csv', header=None, index_col=0).T.reindex(columns=algorithms)
-cath = pd.read_csv('cath.csv', header=None, index_col=0).T.reindex(columns=algorithms)
-#coma = pd.read_csv('coma.csv', header=None, index_col=0).T.reindex(columns=algorithms)
-tesma = pd.read_csv('tesma.csv', header=None, index_col=0).T.reindex(columns=algorithms)
-
-
-dataset_means = {'scop': scop.mean(),
-                 'biosql': biosql.mean(),
-                 'cath': cath.mean(),
-                 #'coma': coma.mean(),
-                 'tesma': tesma.mean()}
+dataset_by_name = dict(zip(datasets, dataset_list))
+dataset_means = {name: dataset.mean() for (name, dataset) in dataset_by_name.items()}
 
 
 combined = pd.concat(dataset_means, axis=1).reindex(columns=datasets)
@@ -39,30 +59,36 @@ columncount = pd.read_csv("columncount.csv", header=None, index_col=0).T
 
 distinct = pd.read_csv("distinct_values.csv", header=None, index_col=0).T.reindex(columns=['demarchi', 'spider'])
 
-DEBUG = True
-
-
+DEBUG = False
+        
 def setup_axes(use_seconds=True):
     #plt.style.use(['fast'])
     if not DEBUG:
         plt.gcf().set_size_inches(8, 4)
+        #plt.gcf().set_size_inches(8, 10)
         plt.gcf().set_dpi(800)
     #formatter = FuncFormatter(lambda x, p: '{:,}'.format(x))
     if use_seconds:
         formatter = FuncFormatter(lambda x, p: str(int(x / 1000)))
         plt.gca().yaxis.set_major_formatter(formatter)
+    
+    plt.gca().yaxis.grid()
+    plt.gca().set_axisbelow(True) # draw grid below lines / bars
 
 
-def export(name):
+def export(name):    
     path = 'export'
     if not os.path.exists(path):
         os.mkdir(path)
-    plt.savefig('%s/%s.png' % (path, name), format='png')
+    plt.savefig('%s/%s.png' % (path, name),
+                format='png',
+                bbox_inches='tight')
     
     if DEBUG:
         plt.show()
     plt.clf()
 
+#%%
 
 def plot_algorithm(algorithm_name, algorithm):
     global combined
@@ -80,6 +106,11 @@ def plot_algorithm(algorithm_name, algorithm):
     export(algorithm)
 
 
+#for name, algorithm in zip(algorithm_names, algorithms):
+#    plot_algorithm(name, algorithm)
+
+#%%
+
 def plot_dataset(dataset_name, dataset):
     global combined
     setup_axes()
@@ -90,37 +121,65 @@ def plot_dataset(dataset_name, dataset):
     data = combined[dataset]
     x = np.arange(len(algorithms))
     plt.xticks(x, algorithm_names)
-    print(x)
-    print(data)
     plt.bar(x, data, width=.2)
     
     export(dataset)
+
+
+#for name, dataset in zip(dataset_names, datasets):
+#    plot_dataset(name, dataset)
  
+#%%
     
 def plot_combined_per_dataset():
     global combined    
     data = combined.transpose()
+    m = data.max(axis=1)
+    data = data.divide(m, axis=0)
+    
+    #data.fillna(1, inplace=True)
 
-    setup_axes()
+    setup_axes(use_seconds=False)
     plt.title('Runtimes')
     plt.xlabel('Dataset')
-    plt.ylabel('Runtime (s)')
+    plt.ylabel('Runtime in % of longest run')
     
     x = np.arange(len(datasets))
     
     ax = plt.subplot(111)
-    b0 = ax.bar(x - 0.4, data['demarchi'], width=.2)
-    b1 = ax.bar(x - 0.2, data['spider'], width=.2)
-    b2 = ax.bar(x + 0, data['sindd'], width=.2)
-    b3 = ax.bar(x + 0.2, data['bb'], width=.2)
-    b4 = ax.bar(x + 0.4, data['binder'], width=.2)
     
-    plt.xticks(x, dataset_names)
-    plt.legend([b0, b1, b2, b3, b4],
-               algorithm_names)
+    bars = []
+    w = .1
+    pad = .2
+    x = np.arange(0,  # start at zero
+                  len(datasets) * w * len(algorithms) + len(datasets) * pad, # total width
+                  .1 * len(algorithms) + pad)
+    x = x[:-1]
+    for index, algo in enumerate(data.columns):
+        bar = ax.bar(x + w * index, data[algo], width=w, align='edge')
+        bars.append(bar)
+        
+    plt.legend(bars, algorithm_names)
+        
+    """
+    w = .1
+    b0 = ax.bar(x - 0.4, data['demarchi'], width=w, align='edge')
+    b1 = ax.bar(x - 0.2, data['spider'], width=w)
+    b2 = ax.bar(x + 0, data['sindd'], width=w)
+    b3 = ax.bar(x + 0.2, data['bb'], width=w)
+    b4 = ax.bar(x + 0.4, data['binder'], width=w)
+    """
+    
+    plt.xticks(x, dataset_names, rotation=45)
+    #plt.legend([b0, b1, b2, b3, b4],
+    #           algorithm_names)
     
     export('combined_per_dataset')
 
+
+plot_combined_per_dataset()
+
+#%%
 
 def plot_combined_per_algorithm():
     global combined
@@ -146,6 +205,9 @@ def plot_combined_per_algorithm():
     export('combined_per_algorithm')
 
 
+plot_combined_per_algorithm()
+#%%
+
 def plot_sindd_partitions():
     global sindd_partitions_scop, sindd_partititions_cath, sindd_paritions_biosql
     
@@ -170,7 +232,10 @@ def plot_sindd_partitions():
     _plot_single('S-INDD Partition Performance: BIOSQL',
                  'sindd_partition_performance_biosql',
                  sindd_partitions_biosql)
-    
+
+
+plot_sindd_partitions()
+#%%
 
 def plot_rowcount_all():
     global rowcount
@@ -182,8 +247,8 @@ def plot_rowcount_all():
     
     counts = np.arange(30000, 330000, 30000)
     ref = []
-    for col in rowcount.columns:
-        ax = plt.plot(counts, rowcount[col])
+    for color, marker, col in zip(algorithm_colors, algorithm_markers, rowcount.columns):
+        ax = plt.plot(counts, rowcount[col], marker=marker, color=color)
         ref.append(ax[0])
     
     plt.legend(ref, algorithm_names)
@@ -191,6 +256,9 @@ def plot_rowcount_all():
     plt.ylim(ymin=0)
     export('rowcount_all')
 
+
+plot_rowcount_all()
+#%%
 
 def plot_rowcount():
     global rowcount
@@ -201,17 +269,21 @@ def plot_rowcount():
     plt.ylabel('Runtime (s)')
     
     counts = np.arange(30000, 330000, 30000)
-    cols = ['spider', 'sindd']
+    cols = ['spider', 'sindd', 'binder']
     ref = []
     for col in cols:
-        ax = plt.plot(counts, rowcount[col])
+        ax = plt.plot(counts, rowcount[col], marker=algorithm_to_marker[col],
+                      color=algorithm_to_color[col])
         ref.append(ax[0])
     
-    plt.legend(ref, ['SPIDER', 'S-INDD'])
+    plt.legend(ref, ['SPIDER', 'S-INDD', 'BINDER'])
     
     plt.ylim(ymin=0)
     export('rowcount')
 
+
+plot_rowcount()
+#%%
 
 def plot_columncount():
     global columncount
@@ -223,8 +295,8 @@ def plot_columncount():
     
     counts = np.arange(18, 108, 18)
     ref = []
-    for col in columncount.columns:
-        ax = plt.plot(counts, columncount[col])
+    for marker, col in zip(algorithm_markers, columncount.columns):
+        ax = plt.plot(counts, columncount[col], marker=marker)
         ref.append(ax[0])
     
     plt.legend(ref, algorithm_names)
@@ -233,6 +305,8 @@ def plot_columncount():
     export('columncount')
 
 
+plot_columncount()
+#%%
 
 def plot_distinct():
     global distinct
@@ -252,7 +326,10 @@ def plot_distinct():
     
     plt.ylim(ymin=0)
     export('distinct')
-        
+
+
+plot_distinct()
+#%%
 
 # hackedihack
 def plot_runtime_shares():
@@ -276,6 +353,9 @@ def plot_runtime_shares():
     export('runtime_contingents')
 
 
+plot_runtime_shares()
+#%%
+
 def plot_query_runtime():
     raw_data = {'not_in': [50],
                 'not_exists': [170],
@@ -283,7 +363,7 @@ def plot_query_runtime():
     
     data = pd.DataFrame(raw_data).reindex(columns=['not_in', 'not_exists', 'left_outer_join']).transpose()
     
-    setup_axes()
+    setup_axes(use_seconds=False)
     plt.title('Query Runtimes')
     plt.xlabel('Query')
     plt.ylabel('Runtime (s)')
@@ -293,25 +373,7 @@ def plot_query_runtime():
     
     plt.xticks(x, ['NOT IN', 'NOT EXISTS', 'LEFT OUTER JOIN'])
     export('query_runtimes')
-    
 
-if __name__ == '__main__':
 
-    for name, dataset in zip(dataset_names, datasets):
-        plot_dataset(name, dataset)
-    
-    for name, algorithm in zip(algorithm_names, algorithms):
-        plot_algorithm(name, algorithm)
-    
-    plot_combined_per_dataset()
-    plot_combined_per_algorithm()
-    
-    plot_sindd_partitions()
-    plot_rowcount_all()
-    plot_rowcount()
-    plot_columncount()
-    plot_distinct()
-    
-    #plot_runtime_shares()
-    #plot_query_runtime()
+plot_query_runtime()
     
